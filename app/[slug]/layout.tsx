0518c1c0bf461170972f6bcb6f5b5ae3a6eaa9bb
Rafael Claro <rafaelpclaroo@gmail.com>
2026-07-08 09:55:34 -0300

    fix: remove default favicon.ico so emoji icon shows on establishment pages

--- app/[slug]/layout.tsx
+++ app/[slug]/layout.tsx
@@ -15,10 +15,12 @@ export async function generateMetadata({
     select: { name: true },
   })
 
+  const svg = `<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🩺</text></svg>`
+
   return {
     title: establishment?.name ?? "AgendaWeb",
     icons: {
-      icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🩺</text></svg>",
+      icon: `data:image/svg+xml;charset=utf-8,${encodeURIComponent(svg)}`,
     },
   }
 }
